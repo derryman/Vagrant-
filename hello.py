eab95af2b35f8979aa0ed32c13ff0608d7188a4b
derryman <derrymahon@icloud.com>
2024-10-04 13:55:53 +0100

    Inital commit add flask app and vagrant file

---
+++ hello.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<p>Hello, World! <a href="/about">About</a></p>'
+
+@app.route('/about')
+def about():
+    return '<p>This is a Flask web app running in a Linux VM.</p>'
